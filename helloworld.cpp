a8b07dd944ce417f1c6922348d0b41a50df7e193
PurpleDerg <btgmns@gmail.com>
2021-02-03 14:20:24 -0400

    Adding simple instructions

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,5 +4,7 @@ using namespace std;
 
 int main() {
     cout << "hello world" << endl;
+    cout << "I am trying to relearn and practice Github and you're suppose to be able to see this in your GIT account" << endl;
+    
     return 0;
 }
